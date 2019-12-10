12a012e16991630cd06265290f49fe74d49f2603
Thomas Randell <thomas.randell@kennedyslaw.com>
2019-12-10 09:23:49 +0000

    removed test code in index file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-import simpleSchema from './_dummy-json/schema-example.schema.json';
-import simpleSchema2 from './_dummy-json/schema-example2.schema.json';
-import kubeSchema from './_dummy-json/kubernetes-example.scehma.json';
-import { Generator } from './generator.js';
-
-const json = [
-  { schema: simpleSchema, title: 'Simple Schema' },
-  { schema: simpleSchema2, title: 'Simple Schema 2' },
-  { schema: kubeSchema, title: 'Kubernetes v1.9.9-standalone/nodelist.json' },
-];
-const outputs = new Generator().generateMarkdown(json);
-
-const output = outputs.map(x => x.output).join('\r\n\r\n');
-const foo = 'end';
+export { Generator } from './generator';
+export { IMarkdownOutput, StyleType, markdownStyles } from './styles';
